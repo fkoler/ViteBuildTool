3c8119c9cc06f24cd34e66174758f03d818bba54
Ferenc Koler <ferenckoler@outlook.com>
2024-07-11 20:39:54 +0200

    Env Variables

--- 05.ReactVite/src/main.jsx
+++ 05.ReactVite/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+console.log(`Server is running in '${import.meta.env.MODE}' mode`);
+console.log(import.meta.env);
+
+if (import.meta.env.DEV) {
+    console.log('This is Dev Server');
+}
+
+if (import.meta.env.VITE_SOME_KEY) {
+    console.log(import.meta.env.VITE_SOME_KEY);
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
