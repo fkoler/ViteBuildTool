112c7b3754fcd37cde97716a78fb4beb9d219452
Ferenc Koler <ferenckoler@outlook.com>
2024-07-12 14:23:12 +0200

    Configurations #2

--- 05.ReactVite/src/main.jsx
+++ 05.ReactVite/src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-console.log(`Server is running in '${import.meta.env.MODE}' mode`);
-console.log(import.meta.env);
+// console.log(`Server is running in '${import.meta.env.MODE}' mode`);
+// console.log(import.meta.env);
 
-if (import.meta.env.DEV) {
-    console.log('This is Dev Server');
-}
+// if (import.meta.env.DEV) {
+//     console.log('This is Dev Server');
+// }
 
-if (import.meta.env.VITE_SOME_KEY) {
-    console.log(import.meta.env.VITE_SOME_KEY);
-}
+// if (import.meta.env.VITE_SOME_KEY) {
+//     console.log(import.meta.env.VITE_SOME_KEY);
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
--- 05.ReactVite/vite.config.js
+++ 05.ReactVite/vite.config.js
@@ -1,1 +1,13 @@
-export default {};
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ command, mode, ssrBuild }) => {
+    console.log(command, mode, ssrBuild);
+
+    if (mode === 'production') {
+        return {
+            base: '/vite/',
+        };
+    }
+
+    return {};
+});
